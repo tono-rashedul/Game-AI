e9ddb198e0230ee4828c892acfc1538f5963171a
tono-rashedul <tono.rashedul@gmail.com>
2023-11-24 06:27:33 +0600

    UI Design

--- game.py
+++ game.py
@@ -1,4 +1,10 @@
 import numpy as np
+import pygame
+import sys
+
+#RGB value
+BLUE = (0,0, 255)  
+BLACK = (0,0,0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
@@ -44,6 +50,12 @@ def winning_move(board, piece):#check horizontal location for win
         for r in range(3, ROW_COUNT):
             if board[r][c] == piece and board [r-1] [c+1] == piece and board [r-2][c+2] == piece and board [r-3][c+3] == piece:
                 return True
+            
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE,(c* SQUARESIZE, r* SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+ SQUARESIZE/2), r* SQUARESIZE+SQUARESIZE+SQUARESIZE/2), RADIUS)
 
 
 board = create_board()
@@ -51,32 +63,27 @@ print_board(board)
 game_over= False
 turn = 0
 
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT *SQUARESIZE
+height =(ROW_COUNT +1 ) * SQUARESIZE
+
+size = (width, height)
+RADIUS = int(SQUARESIZE/2 -5)
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+
+
+
 while not game_over:
-    #Ask for player 1 Input
-    if turn == 0:
-        col = int(input("Player 1 Make your selection (0-6):"))
-        
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 1)
 
-            if winning_move(board, 1):
-                print("Player 1 Wins!! Congratulations!!")
-                game_over = True
-    
-    #Ask for player  2 input
-    else:
-        col = int(input("Player 2 Make your selection (0-6):"))
-        
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 2)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-            if winning_move(board, 2):
-                print("Player 2 Wins!! Congratulations!!")
-                game_over = True
-               
-    print_board(board)
-
-    turn +=1
-    turn = turn %2
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            continue
